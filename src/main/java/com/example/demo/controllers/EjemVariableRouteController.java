3cd02419bff852124e87f015508905e27ae4ad43
Drinkmylk <ivanchris123456789@gmail.com>
2023-05-26 23:34:49 -0500

    The modification was integrated with 2 variables

--- src/main/java/com/example/demo/controllers/EjemVariableRouteController.java
+++ src/main/java/com/example/demo/controllers/EjemVariableRouteController.java
@@ -9,10 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/variables")
 public class EjemVariableRouteController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("title","enviar parametros de la ruta(@PathVariable)");
+        return "variables/index";
+    }
+
     @GetMapping("/string/{text}")
     public String variables(@PathVariable String text , Model model){
         model.addAttribute("title","Recibir parametros de la ruta(@PathVariable)");
         model.addAttribute("result","El texto enviado es ".concat(text) );
+        return "/variables/see";
+    }
+    @GetMapping("/string/{text}/{number}")
+    public String variables(@PathVariable String text, @PathVariable Integer number , Model model){
+        model.addAttribute("title","Recibir parametros de la ruta(@PathVariable)");
+        model.addAttribute("result","El texto enviado es ".concat(text) .concat(" y el numero enviado en el path es : " .concat(number.toString())) );
         return "/variables/see";
     }
 
